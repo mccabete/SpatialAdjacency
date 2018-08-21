dda473372faf9828a944aedc931fb4c8f3ad2600
Tess McCabe <tmccabe@bu.edu>
2018-08-21 12:55:45 -0400

    tiny improvements to get_ecoregion

--- get_ecoregions.R
+++ get_ecoregions.R
@@ -11,13 +11,14 @@ require(igraph)
 eco_II<-rgdal::readOGR("~/Documents/work/na_cec_eco_l2/NA_CEC_Eco_Level2.shp")
 Southern_coastal_plains <- eco_II[eco_II$NA_L2NAME == "MISSISSIPPI ALLUVIAL AND SOUTHEAST USA COASTAL PLAINS", ]
 
-r <- raster(nrow=1e3, ncol=1e3, crs=proj4string(Southern_coastal_plains))
-r[] <- 1:length(r)
+#r <- raster(nrow=1e3, ncol=1e3, crs=proj4string(Southern_coastal_plains))
+#r[] <- 1:length(r)
 
 
 
 Florida <- raster("/Users/tess/Downloads/LANDFIRE/Florida/lf44157424_US_DIST2014/US_DIST2014\\US_DIST2014.tif", nrow=1e6, ncol=1e6, crs=proj4string(Southern_coastal_plains))
 projection(Florida) <- projection(Southern_coastal_plains)
+projection(Florida) <- proj4string(Southern_coastal_plains)
 
 Florida_subset <- crop(Florida, extent(Southern_coastal_plains))
 plot(Florida_subset)
