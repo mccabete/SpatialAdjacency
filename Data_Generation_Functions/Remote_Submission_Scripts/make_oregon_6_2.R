a15133708a9a09d145bac7d50aa5dc3b243090cd
Tess McCabe <tmccabe@bu.edu>
2018-11-12 17:45:19 -0500

    Remote submission & ecoregion subsetting scripts

---
+++ Data_Generation_Functions/Remote_Submission_Scripts/make_oregon_6_2.R
@@ -0,0 +1,21 @@
+library(rgdal)
+library(raster)
+
+florida_patch <- raster::raster("/usr3/graduate/tmccabe/mccabete/spatialadjacentcy/LANDFIRE/Oregon/Oregon_whole_state_2014.tif")
+
+
+eco_II<- rgdal::readOGR("/usr3/graduate/tmccabe/mccabete/spatialadjacentcy/LANDFIRE/na_cec_eco_l2/NA_CEC_Eco_Level2.shp")
+
+
+Southern_coastal_plains <- eco_II[eco_II$NA_L2NAME == "WESTERN CORDILLERA", ]
+#plot(Southern_coastal_plains)
+
+Southern_coastal_plains <- spTransform(Southern_coastal_plains, crs(florida_patch))
+crs(Southern_coastal_plains) <- crs(florida_patch)
+
+
+
+croped <- crop(florida_patch, extent(Southern_coastal_plains))
+masked <- mask(florida_patch, mask = Southern_coastal_plains, inverse=TRUE)
+
+writeRaster(masked, "/usr3/graduate/tmccabe/mccabete/spatialadjacentcy/LANDFIRE/Oregon/Oregon_6_2_2014.tif")
